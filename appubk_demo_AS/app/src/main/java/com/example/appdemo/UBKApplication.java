345abe1221aed224c67a88130026c42c6a8af1e6
feelwx <feeling.123>
2015-11-27 13:46:35 +0800

    version 1.0.53

---
+++ appubk_demo_AS/app/src/main/java/com/example/appdemo/UBKApplication.java
@@ -0,0 +1,21 @@
+package com.example.appdemo;
+
+import android.app.Application;
+
+import com.feelwx.ubk.sdk.ui.component.api.UBKAd;
+
+public class UBKApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		UBKAd.initialize(this);
+		UBKAd.setDebug(true);  // 调试模式，正式发布注释改行
+	}
+
+	@Override
+	public void onTerminate() {
+		UBKAd.destory();
+		super.onTerminate();
+	}	
+}
